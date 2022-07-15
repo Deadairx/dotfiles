3cbf042671cfaa2003af1cf5ccdfb878278a787c
Cody Arnold <carnold@bluebeam.com>
2022-07-15 13:23:33 -0500

    Add brew-app-installs

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Be sure `dotfiles` is **cloned to the home directory**
 
+## Get up and running
+- `brew-app-installs.sh` installs brew and gets all the apps I need through brew
+
 ## Install Stow
 `brew install stow`
 
---
+++ brew-app-installs.sh
@@ -0,0 +1,35 @@
+# Is Brew installed?
+if ! which brew > /dev/null; then
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # add to path
+  echo "Adding Homebrew to PATH"
+  echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+  source ~/.bash_profile
+fi
+
+echo "Installing Homebrew packages"
+
+echo "===Workflow Essentials==="
+brew install --cask iterm2
+brew install --cask docker
+brew install --cask rectangle
+brew install --cask karabiner-elements
+brew install yqrashawn/goku/goku
+brew install --cask visual-studio-code
+
+echo "===Development==="
+brew install npm
+brew install node
+brew install tldr
+brew install minikube
+brew install kubectl
+brew install golang
+brew install tig
+
+echo "===Applications==="
+brew install --cask spotify
+brew install --cask tuple
+brew install --cask vlc
+

c3f7980cfec7c08a827b832a9c6277e418a22500
DeadAirX <djdivinity7@gmail.com>
2022-10-05 09:47:30 -0500

    Add Rust LSP and clean up keybindings

--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -1,33 +1,30 @@
 -- Native LSP Setup
+
+local commonLspKeymaps = function()
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', 'gT', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>dj', '<cmd>lua vim.diagnostic.goto_next()<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>dk', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<CR>', { buffer = 0, noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { buffer = 0, noremap = true, silent = true })
+end
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 require'lspconfig'.gopls.setup{ -- connect to GO LSP server
     capabilities = capabilities,
-    on_attach = function() -- runs on_attach of each buffer
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gT', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>dj', '<cmd>lua vim.diagnostic.goto_next()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>dk', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { buffer = 0, noremap = true, silent = true })
-    end,
+    on_attach = commonLspKeymaps,
 } 
 require'lspconfig'.tsserver.setup{ -- connect to TS LSP server
     capabilities = capabilities,
-    on_attach = function() -- runs on_attach of each buffer
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gT', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>dj', '<cmd>lua vim.diagnostic.goto_next()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>dk', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<CR>', { buffer = 0, noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { buffer = 0, noremap = true, silent = true })
-    end,
+    on_attach = commonLspKeymaps,
 }
+require'lspconfig'.rust_analyzer.setup{ -- connect to Rust LSP server
+    capabilities = capabilities,
+    on_attach = commonLspKeymaps,
+} 
 
 -- LSP autocomplete
 vim.opt.completeopt={"menu", "menuone", "noselect"} -- setting vim completion options

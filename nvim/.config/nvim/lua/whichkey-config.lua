7f0fb3206333d0f4fafb30a5b2f7be0abd831238
DeadAirX <djdivinity7@gmail.com>
2023-03-17 20:49:38 -0500

    a slightly better which-key config

--- nvim/.config/nvim/lua/whichkey-config.lua
+++ nvim/.config/nvim/lua/whichkey-config.lua
@@ -1,1 +1,5 @@
--- ¯\_(ツ)_/¯
+require("which-key").setup {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }

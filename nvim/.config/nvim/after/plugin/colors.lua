790752712750af5a4b07670ea886178d4da7fdc1
Cody Arnold <carnold@bluebeam.com>
2023-12-21 16:10:39 -0600

    vim: shortcuts for colorscheme switching

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,6 +1,18 @@
 function ColorMyPencils(color)
+    vim.cmd("colorscheme " .. color)
+
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+ColorMyPencils("gruvbox") -- Default colorscheme
+
+--vim.api.nvim_set_keymap("n", "<leader>cu", "", { callback = ColorMyPencils("gruvbox"), noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>cu", ":colorscheme gruvbox<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ce", ":colorscheme PaperColor<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>co", ":colorscheme toast<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ca", ":colorscheme everforest<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>cl", ":set background=light<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>cd", ":set background=dark<CR>", { noremap = true, silent = true })
+

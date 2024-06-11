c8859adb18c70f2123859d681a46a09e8bdfe0d2
Cody Arnold <carnold@bluebeam.com>
2024-06-11 15:10:56 -0500

    nvim: seperate plugin config to files

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,10 @@
 return {
 	{
+		-- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"ayu-theme/ayu-vim",
 		"ellisonleao/gruvbox.nvim",
 		"altercation/vim-colors-solarized",
@@ -8,5 +13,37 @@ return {
 		"sainnhe/everforest",
 		"jdkanani/vim-material-theme",
 		"jsit/toast.vim",
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			-- NOTE: colorscheme is set in `after/plugin/color.lua`
+			vim.cmd.colorscheme("tokyonight-night")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+
+	{
+		-- Highlight todo, notes, etc in comments
+		-- Examples:
+		-- FIX:
+		-- BUG:
+		-- ISSUE:
+		-- TODO:
+		-- HACK:
+		-- WARN:
+		-- WARNING:
+		-- PERF:
+		-- NOTE:
+		-- TEST:
+
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = true },
 	},
 }
---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,20 @@
+-- Git Related Plugins
+return {
+	{ -- git "TUI" in nvim
+		"tpope/vim-fugitive",
+		-- open using <leader>gs (for git status)
+	},
+	-- See `:help gitsigns` to understand what the configuration keys do
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+}

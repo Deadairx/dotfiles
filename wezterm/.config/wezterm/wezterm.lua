a7b64059a9383d3dc0c2e03eb5e09db65300d6f5
DeadAirX <djdivinity7@gmail.com>
2024-10-03 21:47:15 -0500

    WezTerm: simplified colors

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,35 +2,21 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font "Comic Mono"
-config.font_size = 18.0
+config.font_size = 24.0
 config.window_background_opacity = 0.8
 
--- config.color_scheme = 'Github'
-config.window_background_gradient = {
-  colors = { '#00a213', '#000203' },
-  -- colors = { 'black' },
-  -- preset = "CubeHelixDefault",
-  orientation = {
-    -- TODO: Experiment with Linear
-    Radial = {
-      -- Specifies the x coordinate of the center of the circle,
-      -- in the range 0.0 through 1.0.  The default is 0.5 which
-      -- is centered in the X dimension.
-      cx = 0.75,
-
-      -- Specifies the y coordinate of the center of the circle,
-      -- in the range 0.0 through 1.0.  The default is 0.5 which
-      -- is centered in the Y dimension.
-      cy = 0.5,
-
-      -- Specifies the radius of the notional circle.
-      -- The default is 0.5, which combined with the default cx
-      -- and cy values places the circle in the center of the
-      -- window, with the edges touching the window edges.
-      -- Values larger than 1 are possible.
-      radius = 1.0,
-    },
-  },
+config.colors = {
+  background = '#202020',
 }
+
+-- config.color_scheme = 'Github'
+-- config.window_background_gradient = {
+--   colors = { 'black', 'gray', '#00a213', 'black' },
+--   -- colors = { 'black' },
+--   -- preset = "CubeHelixDefault",
+--   orientation = 'Vertical',
+--   -- orientation = { Radial = { cx = 0.75, cy = 0.5, radius = 1.0, }, },
+--   blend = 'LinearRgb',
+-- }
 
 return config

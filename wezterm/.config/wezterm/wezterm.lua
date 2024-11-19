cf0bc5f2195cbe37f5e3743305cdcb4d005794b5
Cody Arnold <carnold@bluebeam.com>
2024-11-18 21:14:29 -0600

    wezterm: Add green radial background

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,21 +2,24 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font "Comic Mono"
-config.font_size = 24.0
-config.window_background_opacity = 0.8
-
-config.colors = {
-  background = '#202020',
-}
+config.font_size = 20.0
+-- config.window_background_opacity = 0.8
+config.window_background_opacity = 1.0
 
 -- config.color_scheme = 'Github'
--- config.window_background_gradient = {
---   colors = { 'black', 'gray', '#00a213', 'black' },
---   -- colors = { 'black' },
---   -- preset = "CubeHelixDefault",
---   orientation = 'Vertical',
---   -- orientation = { Radial = { cx = 0.75, cy = 0.5, radius = 1.0, }, },
---   blend = 'LinearRgb',
--- }
+config.window_background_gradient = {
+  colors = { '#005233', '#000203', '#005233' },
+  -- colors = { 'black' },
+  -- preset = "CubeHelixDefault",
+  orientation = {
+    -- TODO: Experiment with Linear
+    Radial = {
+      cx = 0.75,
+      cy = 1.05,
+      radius = 1.2,
+    },
+  },
+}
+
 
 return config

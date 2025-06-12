a7f6c385de90de275a176776234a7fc4aa1894e3
DeadAirX <djdivinity7@gmail.com>
2025-06-12 09:42:25 -0500

    nvim: WIP clojure plugins

---
+++ nvim/.config/nvim/lua/plugins/clojure.lua
@@ -0,0 +1,12 @@
+-- TODO: enable when you get Lisp curious again
+return {
+	{
+		--"tpope/vim-fireplace",
+		-- "Olical/conjure",
+		-- dependencies = {
+		-- 	"tpope/vim-dispatch",
+		-- 	"clojure-vim/vim-jack-in",
+		-- 	"radenling/vim-dispatch-neovim",
+		-- },
+	},
+}
